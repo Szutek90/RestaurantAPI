404cf1030f2235618c9c92e754f5bff5ba5472a6
szutkopawel@gmail.com <szutkopawel@gmail.com>
2023-03-01 15:41:21 +0100

    Zrobiona autentykacja i logowanie

---
+++ RestaurantAPI/AuthenticationSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantAPI
+{
+    public class AuthenticationSettings
+    {
+        public string JwtIssuer { get; set; }
+        public string JwtAudience { get; set; }
+        public int JwtExpireDays { get; set; }
+        public string JwtKey { get; set; }
+    }
+}
