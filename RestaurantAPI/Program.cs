4df4d9d90ca96eebf2db04433bf51c19bb6aeb53
szutkopawel@gmail.com <szutkopawel@gmail.com>
2023-03-01 13:04:16 +0100

    test powrotu

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -1,27 +1,14 @@
 using RestaurantAPI;
 using RestaurantAPI.Entities;
-using RestaurantAPI.Services;
-using System.Reflection;
-using NLog;
-using NLog.Web;
-using RestaurantAPI.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddTransient<IWeatherForecastService, WeatherForecastService>();
 builder.Services.AddDbContext<RestaurantDbContext>();
 builder.Services.AddScoped<RestaurantSeeder>();
-builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-builder.Services.AddScoped<IRestaurantService, RestaurantService>();
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
-builder.Services.AddScoped<RequestTimeMiddleware>();
-builder.Services.AddSwaggerGen();
-
-// NLog: Setup NLog for Dependency injection
-builder.Logging.ClearProviders();
-builder.Host.UseNLog();
 
 var app = builder.Build();
 var scope = app.Services.CreateScope();
@@ -30,20 +17,8 @@ var seeder = scope.ServiceProvider.GetRequiredService<RestaurantSeeder>();
 // Configure the HTTP request pipeline.
 seeder.Seed();
 
-app.UseMiddleware<ErrorHandlingMiddleware>();
-app.UseMiddleware<RequestTimeMiddleware>();
-
 app.UseHttpsRedirection();
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Restaurant Api");
-    });
-}
 
-app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
