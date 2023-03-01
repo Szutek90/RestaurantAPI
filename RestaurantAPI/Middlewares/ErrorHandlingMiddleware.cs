5306b3cffb8fd335ca044b111f3a90bfbbeb8dc9
szutkopawel@gmail.com <szutkopawel@gmail.com>
2023-03-01 17:49:42 +0100

    Revert "Zrobiona autentykacja i logowanie"
    This reverts commit 3d2487e0468358a8a66345158d758d7cc05d6509.

--- RestaurantAPI/Middlewares/ErrorHandlingMiddleware.cs
+++ RestaurantAPI/Middlewares/ErrorHandlingMiddleware.cs
@@ -16,12 +16,6 @@ namespace RestaurantAPI.Middlewares
             {
                 await next.Invoke(context);
             }
-            catch(BadRequestException badRequestException)
-            {
-                context.Response.StatusCode = 400;
-                await context.Response.WriteAsync(badRequestException.Message);
-            }
-
             catch(NotFoundException notFoundException)
             {
                 context.Response.StatusCode = 404;
